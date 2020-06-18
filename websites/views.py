8f5660e264a9f1633f396def7cecb08e6388fb50
tsukasa <renato.tsukasa@gmail.com>
2020-06-18 13:53:38 -0300

    Home template created

--- websites/views.py
+++ websites/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
+
+class Index(View):
+
+    def get(self, *args, **kwargs):
+        return render(self.request, 'website.html')
