509875d056cf993454e56b18d5751585ed5238c6
Renato <tsukasa.renato@gmail.com>
2020-12-08 18:32:56 -0300

    removing customizations in admin

--- websites/admin.py
+++ websites/admin.py
@@ -2,46 +2,11 @@ from django.contrib import admin
 from . import models
 
 
-class ColorsInline(admin.TabularInline):
-    model = models.Colors
-
-
-class IconsInline(admin.TabularInline):
-    model = models.Icons
-
-
-class WebsitesAdmin(admin.ModelAdmin):
-    list_display = ['url', 'get_created_at', 'get_updated_at']
-    inlines = [
-        ColorsInline,
-        IconsInline
-    ]
-
-
-class OptionsInline(admin.TabularInline):
-    model = models.Options
-
-
-class GroupsAdmin(admin.ModelAdmin):
-    list_display = ['title', 'slug', 'calculation', 'minimum', 'maximum']
-    inlines = [
-        OptionsInline
-    ]
-
-
-class ProductsAdmin(admin.ModelAdmin):
-    list_display = ['title', 'slug', 'images']
-
-
-class OptionsAdmin(admin.ModelAdmin):
-    list_display = ['title', 'slug', 'images']
-
-
-admin.site.register(models.Websites, WebsitesAdmin)
+admin.site.register(models.Websites)
 admin.site.register(models.Categories)
-admin.site.register(models.Products, ProductsAdmin)
-admin.site.register(models.Groups, GroupsAdmin)
-admin.site.register(models.Options, OptionsAdmin)
+admin.site.register(models.Products)
+admin.site.register(models.Groups)
+admin.site.register(models.Options)
 admin.site.register(models.Banners)
 admin.site.register(models.Contacts)
 admin.site.register(models.Icons)
