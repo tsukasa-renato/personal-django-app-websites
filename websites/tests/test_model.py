6ff08d8fac5da6dd6e889158ecb52e1f6988f6d7
Renato <tsukasa.renato@gmail.com>
2020-12-14 21:49:10 -0300

    Adjusting setUp

--- websites/tests/test_model.py
+++ websites/tests/test_model.py
@@ -3,28 +3,24 @@ from django.db.utils import IntegrityError
 from ..models import Websites, Categories, Products
 
 
-class ProductsModelTest(TestCase):
-
+class SetUpConfig(TestCase):
+    """
+    Register necessary initial data for the tests
+    """
     @classmethod
     def setUpTestData(cls):
         cls.website = Websites.objects.create(url='url', title="title")
         cls.category = Categories.objects.create(websites=cls.website, title="title")
+        cls.product = Products.objects.create(websites=cls.website, categories=cls.category, title="title",
+                                              price_type='1', price=1, position=1)
 
-    def test_register_product(self):
-        """
-        Test a common register
-        """
 
-        p = Products.objects.create(websites=self.website, categories=self.category, title="title", price=1)
-
-        self.assertEqual(p.id, 1)
+class ProductsModelTest(SetUpConfig):
 
     def test_unique_constraints(self):
         """
         Register two products with same website and title (slug)
         """
-
-        Products.objects.create(websites=self.website, categories=self.category, title="title", price=1)
 
         with self.assertRaises(Exception):
             Products.objects.create(websites=self.website, categories=self.category, title="title", price=1)
@@ -35,13 +31,13 @@ class ProductsModelTest(TestCase):
         """
 
         with self.assertRaises(ValueError):
-            Products.objects.create(websites=self.website, categories=self.category, title="title", price_type='1')
+            Products.objects.create(websites=self.website, categories=self.category, title="title_", price_type='1')
 
         with self.assertRaises(ValueError):
-            Products.objects.create(websites=self.website, categories=self.category, title="title", price_type='2')
+            Products.objects.create(websites=self.website, categories=self.category, title="title_", price_type='2')
 
         with self.assertRaises(ValueError):
-            Products.objects.create(websites=self.website, categories=self.category, title="title", price_type='3')
+            Products.objects.create(websites=self.website, categories=self.category, title="title_", price_type='3')
 
     def test_fail_in_check_price_with_price(self):
         """
@@ -49,20 +45,18 @@ class ProductsModelTest(TestCase):
         """
 
         with self.assertRaises(ValueError):
-            Products.objects.create(websites=self.website, categories=self.category, title="title", price_type='4',
+            Products.objects.create(websites=self.website, categories=self.category, title="title_", price_type='4',
                                     price=1)
 
         with self.assertRaises(ValueError):
-            Products.objects.create(websites=self.website, categories=self.category, title="title", price_type='5',
+            Products.objects.create(websites=self.website, categories=self.category, title="title_", price_type='5',
                                     price=1)
 
     def test_success_in_check_price_with_price(self):
         """
-        Register a product with price_type in (1,2,3) and price=1
+        Register a product with price_type in (1,2,3) and price=1, price_type = 1 was registered in setUp
         """
 
-        Products.objects.create(websites=self.website, categories=self.category, title="title", price_type='1',
-                                price=1, position=1)
         Products.objects.create(websites=self.website, categories=self.category, title="title2", price_type='2',
                                 price=1, position=2)
         Products.objects.create(websites=self.website, categories=self.category, title="title3", price_type='3',
@@ -76,13 +70,13 @@ class ProductsModelTest(TestCase):
         Register a product with price_type in (4,5) and price=None
         """
 
-        Products.objects.create(websites=self.website, categories=self.category, title="title", price_type='4',
+        Products.objects.create(websites=self.website, categories=self.category, title="title2", price_type='4',
                                 position=1)
-        Products.objects.create(websites=self.website, categories=self.category, title="title2", price_type='5',
+        Products.objects.create(websites=self.website, categories=self.category, title="title3", price_type='5',
                                 position=2)
 
         self.assertQuerysetEqual(Products.objects.all().order_by('position'),
-                                 ['<Products: title>', '<Products: title2>'])
+                                 ['<Products: title>', '<Products: title2>', '<Products: title3>'])
 
     def test_fail_in_check_promotional_price_without_price(self):
         """
@@ -90,7 +84,7 @@ class ProductsModelTest(TestCase):
         """
 
         with self.assertRaises(ValueError):
-            Products.objects.create(websites=self.website, categories=self.category, title="title", price_type='4',
+            Products.objects.create(websites=self.website, categories=self.category, title="title_", price_type='4',
                                     promotional_price=1)
 
     def test_fail_in_check_promotional_price_with_price(self):
@@ -99,7 +93,7 @@ class ProductsModelTest(TestCase):
         """
 
         with self.assertRaises(ValueError):
-            Products.objects.create(websites=self.website, categories=self.category, title="title", price=1,
+            Products.objects.create(websites=self.website, categories=self.category, title="title_", price=1,
                                     promotional_price=2)
 
     def test_success_in_check_promotional_price(self):
@@ -107,7 +101,8 @@ class ProductsModelTest(TestCase):
         Register a product with promotional price
         """
 
-        Products.objects.create(websites=self.website, categories=self.category, title="title", price=2,
+        Products.objects.create(websites=self.website, categories=self.category, title="title_", price=2,
                                 promotional_price=1)
 
-        self.assertQuerysetEqual(Products.objects.all().order_by('position'), ['<Products: title>'])
+        self.assertQuerysetEqual(Products.objects.all().order_by('position'),
+                                 ['<Products: title>', '<Products: title_>'])
