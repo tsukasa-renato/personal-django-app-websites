6b21e038bd9d3e68e4f303907d56ffc7c2be8b3d
Renato <tsukasa.renato@gmail.com>
2020-12-16 17:13:43 -0300

    Adjusting views and templates

--- websites/views.py
+++ websites/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import View
 from django.core.paginator import Paginator
-from . import models
+from .models import Websites, Categories, Products, Groups, Options, \
+    Banners, Contacts, Icons, Colors
+
+
+def website_configs(website):
+    pass
 
 
 def list_product(**kwargs):
@@ -9,47 +14,49 @@ def list_product(**kwargs):
     There are 3 ways to list the products, 1 by category, 2 by the search bar, 3 by accessing the homepage.
     """
     if kwargs['category']:
-        return models.Products.objects.filter(categories=kwargs['category']).order_by('position')
+        return Products.objects.filter(categories=kwargs['category']).order_by('position')
     if kwargs['search']:
-        return models.Products.objects.filter(websites=kwargs['websites'],
-                                              title__icontains=kwargs['search']).order_by('position')
+        return Products.objects.filter(websites=kwargs['websites'],
+                                       title__icontains=kwargs['search']).order_by('position')
 
-    return models.Products.objects.filter(websites=kwargs['websites'],
-                                          show_home=True).order_by('position')
+    return Products.objects.filter(websites=kwargs['websites'],
+                                   show_home=True).order_by('position')
 
 
 class Home(View):
 
     def get(self, *args, **kwargs):
 
-        websites = models.Websites.objects.get(url=kwargs['url'])
-        contacts = models.Contacts.objects.get(websites=websites)
-        icons = models.Icons.objects.get(websites=websites)
-        colors = models.Colors.objects.get(websites=websites)
-        banners = models.Banners.objects.filter(websites=websites).order_by('position')
-        categories = models.Categories.objects.filter(websites=websites).order_by('position')
+        website = get_object_or_404(Websites, url=kwargs['url'])
+
+        contact = Contacts.objects.filter(websites=website).first()
+        icon = Icons.objects.filter(websites=website).first()
+        color = Colors.objects.filter(websites=website).first()
+
+        banners = Banners.objects.filter(websites=website).order_by('position')
+        categories = Categories.objects.filter(websites=website).order_by('position')
 
         category = ''
         search = self.request.GET.get('search')
 
         if 'category' in kwargs:
-            category = models.Categories.objects.filter(websites=websites, slug=kwargs['category']).first()
+            category = Categories.objects.filter(websites=website, slug=kwargs['category']).first()
 
-        products = list_product(websites=websites, category=category, search=search)
+        products = list_product(websites=website, category=category, search=search)
 
         paginator = Paginator(products, 8)
         page_number = self.request.GET.get('page')
         page_obj = paginator.get_page(page_number)
 
         context = {
-            'websites': websites,
-            'contacts': contacts or '',
-            'icons': icons or '',
-            'colors': colors or '',
-            'banners': banners or '',
-            'categories': categories or '',
-            'category_': category or '',
-            'products': page_obj or '',
+            'websites': website,
+            'contacts': contact,
+            'icons': icon,
+            'colors': color,
+            'banners': banners,
+            'categories': categories,
+            'category_': category,
+            'products': page_obj,
         }
 
         return render(self.request, 'website.html', context)
@@ -59,12 +66,12 @@ class Product(View):
 
     def get(self, *args, **kwargs):
 
-        websites = models.Websites.objects.get(url=kwargs['url'])
-        icons = models.Icons.objects.get(websites=websites)
-        colors = models.Colors.objects.get(websites=websites)
-        product = models.Products.objects.get(websites=websites, slug=kwargs['product'])
-        groups = models.Groups.objects.filter(products=product).order_by('position')
-        options = models.Options.objects.filter(groups__in=groups).order_by('position')
+        websites = Websites.objects.get(url=kwargs['url'])
+        icons = Icons.objects.get(websites=websites)
+        colors = Colors.objects.get(websites=websites)
+        product = Products.objects.get(websites=websites, slug=kwargs['product'])
+        groups = Groups.objects.filter(products=product).order_by('position')
+        options = Options.objects.filter(groups__in=groups).order_by('position')
 
         context = {
             'websites': websites,
@@ -79,12 +86,12 @@ class Product(View):
 
     def post(self, *args, **kwargs):
 
-            websites = models.Websites.objects.get(url=kwargs['url'])
-            icons = models.Icons.objects.get(websites=websites)
-            colors = models.Colors.objects.get(websites=websites)
-            product = models.Products.objects.get(websites=websites, slug=kwargs['product'])
-            groups = models.Groups.objects.filter(products=product).order_by('position')
-            options = models.Options.objects.filter(groups__in=groups).order_by('position')
+            websites = Websites.objects.get(url=kwargs['url'])
+            icons = Icons.objects.get(websites=websites)
+            colors = Colors.objects.get(websites=websites)
+            product = Products.objects.get(websites=websites, slug=kwargs['product'])
+            groups = Groups.objects.filter(products=product).order_by('position')
+            options = Options.objects.filter(groups__in=groups).order_by('position')
 
             context = {
                 'websites': websites,
