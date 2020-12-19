8ff25b43319ea707ea953edb2afafd5e6c4645c3
Renato <tsukasa.renato@gmail.com>
2020-12-19 15:13:17 -0300

    Add readonly in product template and comments

--- websites/static/custom/js/website.js
+++ websites/static/custom/js/website.js
@@ -10,9 +10,14 @@ var div = $('.fixed-top');
 
 function returns_input_html(type, name, id, value, min, max, image, content){
 
+    readonly = ""
+    if (min == max) {
+        readonly = "readonly"
+    }
+
     if (type != "number") {
         return '<div class="form-check mr-2"> ' +
-            '<input class="form-check-input" type='+type+' name='+name+' id='+id+' value='+value+'> ' +
+            '<input class="form-check-input" type='+type+' name='+name+' id='+id+' value='+value+' '+readonly+'> ' +
             '<label class="form-check-label btn-primary rounded p-1" for='+name+'> ' +
                 image +
                 content +
@@ -22,7 +27,7 @@ function returns_input_html(type, name, id, value, min, max, image, content){
 
     return '<div class="form-check mr-2"> ' +
         '<input class="form-control-input mr-1" type='+type+' name='+name+' id='+id+' value='+min+
-        'min='+min+' max='+max+'>' +
+        'min='+min+' max='+max+' style="width:50px;" '+readonly+'>' +
         '<label class="form-control-input btn-primary rounded p-1" for='+name+'>' +
             image +
             content +
