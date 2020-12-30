ed8e443e2376ceea2b845a325e400ff2fdbe1045
tsukasa-renato <tsukasa.renato@gmail.com>
2020-12-30 13:21:06 -0300

    Readonly input implement in product template

--- websites/static/custom/js/website.js
+++ websites/static/custom/js/website.js
@@ -12,17 +12,17 @@ function returns_input_html(type, name, id, min, max, image, content){
 
     readonly = ""
     checked = ""
-    if (min == max) {
-        readonly = "readonly"
-        if (max == 1) {
-            checked = "checked"
-        }
-    }
 
     if (type != "number") {
+
+        if (min == max) {
+            checked = "checked";
+            readonly = "disabled";
+        }
+
         return '<div class="form-check mr-2"> ' +
             '<input class="form-check-input" type='+type+' name='+name+' id='+id+' value='+id+
-            ' '+readonly+' '+checked+'> ' +
+            ' '+checked+' '+readonly+'> ' +
             '<label class="form-check-label btn-primary rounded p-1" for='+id+'> ' +
                 image +
                 content +
@@ -30,6 +30,7 @@ function returns_input_html(type, name, id, min, max, image, content){
         '</div>';
     }
 
+    if (min == max) readonly = "readonly";
     return '<div class="form-check mr-2"> ' +
         '<input class="form-control-input mr-1" type='+type+' name='+name+' id='+id+' '+
         'value='+min+' min='+min+' max='+max+' style="width:50px;" '+readonly+'>' +
